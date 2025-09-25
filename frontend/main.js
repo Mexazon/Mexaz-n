c0f1953fc291158a4408b7f5683e533b549cfd9b
Freddy Rmz <alfredoro000@gmail.com>
2025-09-24 18:19:39 -0600

    fix main.js imported files

--- frontend/main.js
+++ frontend/main.js
@@ -6,8 +6,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // CSS principal
 import './css/styles.css';
+import './css/loged.css';
 
 // Scripts
 import './js/formulario.js';
 import './js/register-wizard.js';
-
+import './js/posts.js';
+import './js/classes.js';
