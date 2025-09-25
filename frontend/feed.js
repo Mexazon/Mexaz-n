6bb32c5d1d55340d5b008345972e8053f8b64438
Freddy Rmz <alfredoro000@gmail.com>
2025-09-24 18:32:53 -0600

    feed.js and button login fixed

--- frontend/feed.js
+++ frontend/feed.js
@@ -5,9 +5,9 @@ import '../sass/custom.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // CSS principal
-import './css/styles.css';
+
+import './css/loged.css';
 
 // Scripts
-import './js/formulario.js';
-import './js/register-wizard.js';
-
+import './js/posts.js';
+import './js/classes.js';
