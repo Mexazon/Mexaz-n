b036ff022bcfbd40ebfbdf26b2af90a4b28e7515
Freddy Rmz <alfredoro000@gmail.com>
2025-09-24 18:23:03 -0600

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   root: 'frontend',
-  base: '/Mexaz-n/',
+  base: command === 'build' ? '/Mexaz-n/' : '/',  // base=/ in dev, /Mexaz-n/ in build
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'frontend/index.html'),
+        feed: resolve(__dirname, 'frontend/feed.html'), // or 'frontend/pages/feed.html'
+      },
+    },
   },
-  server: { open: true }
-});
+  server: { open: true },
+}));
