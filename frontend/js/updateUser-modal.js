05fe70508a040574a60b6bb2d95e9af127589c45
Freddy Rmz <alfredoro000@gmail.com>
2025-10-24 13:23:48 -0600

    checking

---
+++ frontend/js/updateUser-modal.js
@@ -0,0 +1,12 @@
+import{updateUserPartial} from './controllers/putControllers';
+
+const nameInput = document.getElementById("");
+const descriptionInput = document.getElementById("");
+
+document.getElementById('saveProfileBtn').addEventListener('submit', async function(){
+    if(nameInput.value != '' || descriptionInput.value != ''){
+
+    }
+
+})
+
